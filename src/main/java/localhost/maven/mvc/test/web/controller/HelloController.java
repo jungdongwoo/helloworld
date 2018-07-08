572f71fa27545b03e21e21acb57132d661d43bda
jungdongwoo <dwjung@kstargroup.tv>
2018-07-08 11:46:34 +0900

    Initial commit

---
+++ src/main/java/localhost/maven/mvc/test/web/controller/HelloController.java
@@ -0,0 +1,28 @@
+package localhost.maven.mvc.test.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller()
+public class HelloController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printWelcome(Model modle) {
+        modle.addAttribute("message", "Hello world");
+        return "hello";
+    }
+
+    @RequestMapping(value = "/hello/{name:/.+}", method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("hello");
+        model.addObject("msg", name);
+
+        return model;
+
+    }
+}
